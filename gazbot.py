86971730078ddb615ba7cd617ade976035595fb0
arnauddavoutsonos <arnaud.davout@sonos.com>
2023-02-22 18:43:15 +0100

    Simplified code

--- gazbot.py
+++ gazbot.py
@@ -2,7 +2,6 @@ import imaplib
 import email
 from email.message import EmailMessage
 from email.header import decode_header
-import webbrowser
 import os
 import argparse
 
@@ -11,6 +10,9 @@ class GazBot:
     def __init__(self, server, username, password):
         self.imap = imaplib.IMAP4_SSL(server)
         self.imap.login(username, password)
+        self._workspace = 'data'
+        if not os.path.exists(self._workspace):
+            os.mkdir(self._workspace)
 
     def clean(self, text):
         return "".join(c if c.isalnum() else "_" for c in text)
@@ -26,6 +28,7 @@ class GazBot:
         status, messages = self.imap.select("INBOX")
         message_count = int(messages[0])
         message_count_limit = 30
+        gazette_body = ""
 
         for _idx in range(message_count, max(0,message_count-message_count_limit), -1):
             res, msg = self.imap.fetch(str(_idx), "(RFC822)")
@@ -34,48 +37,57 @@ class GazBot:
                 if isinstance(response, tuple):
                     msg = email.message_from_bytes(response[1])
                     subject, encoding = decode_header(msg["Subject"])[0]
+
                     if isinstance(subject, bytes):
                         subject = subject.decode(encoding)
+
                     From, encoding = decode_header(msg.get("From"))[0]
                     if isinstance(From, bytes):
                         From = From.decode(encoding)
+
                     print("Subject:", subject)
                     print("From:", From)
+
                     if msg.is_multipart():
-                        for part in msg.walk():
-                            content_type = part.get_content_type()
-                            content_disposition = part.get_content_disposition()
-                            if content_type == "text/plain" and content_disposition != 'attachment':
-                                if part.get_payload(decode=True) is not None:
-                                    body = part.get_payload(decode=True).decode()
-                                    print('text/plain body:')
-                                    print(body)
-                            elif content_disposition == 'attachment':
-                                filename = self.get_part_filename(part)
-                                if filename:
-                                    folder_name = self.clean(subject)
-                                    if not os.path.isdir(folder_name):
-                                        os.mkdir(folder_name)
-                                    filepath = os.path.join(folder_name, filename)
-                                    print('attachment found:')
-                                    print(filename)
-                                    open(filepath, "wb").write(part.get_payload(decode=True))
+                        message_list = msg.walk()
                     else:
-                        content_type = msg.get_content_type()
-                        body = msg.get_payload(decode=True).decode()
-                        if content_type == "text/plain":
+                        message_list = [msg]
+
+                    for part in message_list:
+                        content_type = part.get_content_type()
+                        content_disposition = part.get_content_disposition()
+
+                        if content_type == "text/html":
+                            filename = self.clean(subject)+'.html'
+                            filepath = os.path.join(self._workspace, filename)
+                            body = part.get_payload(decode=True).decode()
+                            print('text/html body:')
                             print(body)
-                    if content_type == "text/html":
-                        folder_name = self.clean(subject)
-                        if not os.path.isdir(folder_name):
-                            os.mkdir(folder_name)
-                        filename = "index.html"
-                        filepath = os.path.join(folder_name, filename)
-                        print('text/html body:')
-                        print(body)
-                        open(filepath, "w").write(body)
-                        webbrowser.open(filepath)
+                            with open(filepath, "w") as f:
+                                f.write(body)
+                            
+
+                        # elif content_type == "text/plain" and content_disposition != 'attachment':
+                        #     if part.get_payload(decode=True) is not None:
+                        #         filename = self.clean(subject)+'.txt'
+                        #         filepath = os.path.join(self._workspace, filename)
+                        #         body = part.get_payload(decode=True).decode()
+                        #         print('text/plain body:')
+                        #         print(body)
+                        #         with open(filepath, "w") as f:
+                        #             f.write(body)
+
+                        elif content_disposition == 'attachment':
+                            filename = self.get_part_filename(part)
+                            if filename:
+                                filepath = os.path.join(self._workspace, filename)
+                                print('attachment found:')
+                                print(filename)
+                                with open(filepath, "wb") as f:
+                                    f.write(part.get_payload(decode=True))
+
                     print("="*100)
+
         self.imap.close()
         self.imap.logout()
 
